a3da7350a58194037580a2e3858d413f5f2ca721
hlysig <hlysig@gagnavarslan.is>
2012-08-28 12:26:42 +0000

    Fixing setup setup step.
    Now creating symbolic link for vimrc config.

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,41 +1,32 @@
 # Check if vimlicious is already installed.
-if [ -d "$HOME/.vimlicious" ]; then
-    echo "\033[0;31mYou already have vimlicious installed.\033[0m"
-    echo "\033[0;31mRemove ~/.vimlicious and try again.\033[0m"
-    exit
+if [ -d ~/.vimlicious ]; then
+  echo "\033[0;31mEy! You already have vimlicious installed.\033[0m"
+  echo "\033[0;31mRemove ~/.vimlicious and try again.\033[0m"
+  exit
 fi
 
 echo "\033[0;34mCloning vimlicious\033[0m"
 hash git >/dev/null && /usr/bin/env git clone https://github.com/hlysig/vimlicious.git ~/.vimlicious || {
-    echo "\033[0;31mGit is not installed. Get it and run this script again.\033[0m"
-    exit
+echo "\033[0;31mGit is not installed. Get it and run this script again.\033[0m"
+exit
 }
 
 echo "\033[0;34mLooking for an existing vim config...\033[0m"
 if [ -f ~/.vimrc ] || [ -h ~/.vimrc ]
 then
-  echo "\033[0;33mFound ~/.vimrc.\033[0m \033[0;32]Backing up to ~/.vimrc.pre-vimlicious\033[0m";
+  echo "\033[0;34mFound old .vimrc. Backing it up to ~/.vimrc.pre-vimlicious\033[0m"
   cp ~/.vimrc ~/.vimrc.pre-vimlicious;
   rm ~/.vimrc;
 fi
 
-echo "\033[0;34mUsing vimlicious template file and adding it to ~/.vimrc\033[0m"
-cp ~/.vimlicious/templates/vimrc ~/.vimrc
-
-
-echo "\033[0;34mLooking for an existing local vim config...\033[0m"
-if [ -f ~/.vimrc.local ] || [ -h ~/.vimrc.local ]
+echo "\033[0;34mLooking for an existing .vim folder...\033[0m"
+if [ -d ~/.vim ]
 then
-  echo "\033[0;33mFound ~/.vimrc.local.\033[0m \033[0;32]Backing up to ~/.vimrc.local.pre-vimlicious\033[0m";
-  cp ~/.vimrc.local ~/.vimrc.local.pre-vimlicious;
-  rm ~/.vimrc.local;
+  echo "\033[0;34mFound .vim folder. Backing it up to ~/.vim.pre-vimlicious\033[0m"
+  mv ~/.vim ~/.vim.pre-vimlicious;
 fi
 
-echo "\033[0;34mCreating empty .vimrc.local. Place all your vim configuration there.\033[0m"
-touch ~/.vimrc.local
-
 echo "\033[0;34minitializing vimlicious..\033[0m"
-export PATH=$PATH:~/.vimlicious/bin
 ~/.vimlicious/bin/vimlicious init
 
 echo "\033[0;34m"'       _           _ _      _                  '"\033[0m"           
